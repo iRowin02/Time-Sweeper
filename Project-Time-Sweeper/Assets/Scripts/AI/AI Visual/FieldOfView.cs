9f7e68a724ad4608ad790be6a5cca033d2e49c2e
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-01-27 16:55:00 +0100

    FoV Visuals

---
+++ Project-Time-Sweeper/Assets/Scripts/AI/AI Visual/FieldOfView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Rad2Deg),0,Mathf.Cos(angleInDegrees * Mathf.Rad2Deg));
+    }
+}
