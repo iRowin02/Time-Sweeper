731ed4c5a95e65384e92c3b977987c4fe19b0ca5
wtfRowin <r.s.salomons@gmail.com>
2020-01-27 15:58:27 +0100

    EPISCHE GRANAAT

---
+++ Project-Time-Sweeper/Assets/2 - Rowin Folder/Grenade.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float timer;
+    public float radius;
+    public float force;
+    private float countdown;
+    private bool hasExploded;
+    public GameObject particleEffect;
+
+
+    void Start()
+    {
+        countdown = timer;
+    }
+    void Update()
+    {
+        TimerGrenade();
+    }
+    void TimerGrenade()
+    {
+        countdown -= Time.deltaTime;
+
+        if(countdown <= 0 && !hasExploded)
+        {
+            ExplodeGrenade();
+        }
+    }
+    void ExplodeGrenade()
+    {
+        GameObject particle = Instantiate(particleEffect, transform.position, Quaternion.identity);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearObjects in colliders)
+        {
+            Rigidbody rb = nearObjects.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+
+        Object.Destroy(particle, 1);
+        Destroy(gameObject);
+        hasExploded = true;
+    }
+}
