1a1c6517009e4daa29d88730bc3697162c436b91
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-01-28 11:39:54 +0100

    FOV Visuals Update

--- Project-Time-Sweeper/Assets/Editor/FieldOfViewEditor.cs
+++ Project-Time-Sweeper/Assets/Editor/FieldOfViewEditor.cs
@@ -1,20 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEditor;
 
-[CustomEditor (typeof (FieldOfView))]
-public class FieldOfViewEditor : Editor
+[CustomEditor(typeof (FieldOfView))]
+public class FieldOfViewEditor : Editor 
 {
-    public void OnSceneGUI() 
+	void OnSceneGUI() 
     {
-        FieldOfView fov = (FieldOfView)target;
-        Handles.color = Color.white;
-        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
-        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle/2,false);
-        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle/2,false);
+		FieldOfView fow = (FieldOfView)target;
+		Handles.color = Color.white;
+		Handles.DrawWireArc (fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+		Vector3 viewAngleA = fow.DirFromAngle (-fow.viewAngle / 2, false);
+		Vector3 viewAngleB = fow.DirFromAngle (fow.viewAngle / 2, false);
 
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
-    }
+		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+		Handles.color = Color.red;
+
+		foreach(Transform visibleTarget in fow.visibleTargets) 
+        {
+			Handles.DrawLine (fow.transform.position, visibleTarget.position);
+		}
+	}
 }
