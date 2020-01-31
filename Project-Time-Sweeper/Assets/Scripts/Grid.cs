e8e764ebb74a74b079f2f98833f69993082d447a
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-01-31 13:28:15 +0100

    Layers + A*

---
+++ Project-Time-Sweeper/Assets/Scripts/Grid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public LayerMask obstacles;
+    public Vector2 gridWorldSize;
+    public float nodeSize;
+    Node[,] grid;
+
+     void OnDrawGizmos() 
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x,1,gridWorldSize.y));
+    }
+}
