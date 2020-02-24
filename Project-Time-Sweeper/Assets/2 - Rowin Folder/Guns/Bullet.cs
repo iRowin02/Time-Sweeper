7d37d4a0737f1aad2bcb07628adc21f8fc4e95a6
wtfRowin <r.s.salomons@gmail.com>
2020-02-24 20:03:04 +0100

    dfgdh

--- Project-Time-Sweeper/Assets/2 - Rowin Folder/Guns/Bullet.cs
+++ Project-Time-Sweeper/Assets/2 - Rowin Folder/Guns/Bullet.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float bulletspeed;
-    public GameObject bulletHole;
 
     void OnCollisionEnter(Collision coll)
     {
         if(coll.gameObject.GetComponent<Collider>())
         {
             Destroy(gameObject);
-            Instantiate(bulletHole, gameObject.transform.position, coll.gameObject.transform.rotation);
         }
     }
     void Update()
