d5954c41f88d5cdb0a65ba01adc3c5ff27fea8c0
wtfRowin <r.s.salomons@gmail.com>
2020-02-27 13:56:21 +0100

    Deur meuk

---
+++ Project-Time-Sweeper/Assets/2 - Rowin Folder/Door/DoorInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteraction : MonoBehaviour
+{
+    public Animator anim;
+    private int waitfor = 5;
+
+    private void Awake() 
+    {
+        anim = GetComponent<Animator>();     
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(DoorAnim());
+        }
+    }
+    IEnumerator DoorAnim()
+    {
+        anim.SetBool("NearDoor", true);
+
+        yield return new WaitForSeconds(waitfor);
+
+        anim.SetBool("NearDoor", false);
+    }
+}
