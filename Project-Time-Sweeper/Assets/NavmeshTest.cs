2fdf22aec3e535a78be0f73f2cff33a5b7b57b8a
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-03-10 11:37:19 +0100

    AI test

---
+++ Project-Time-Sweeper/Assets/NavmeshTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavmeshTest : MonoBehaviour
+{
+    public Transform t;
+    public NavMeshAgent agent;
+    
+    public void Start() 
+    {
+        agent.SetDestination(t.position);
+    }
+}
