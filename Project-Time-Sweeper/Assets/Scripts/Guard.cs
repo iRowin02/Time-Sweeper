81a2d04edde86ccbedfafcd02e34ae45bffb21e3
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-03-11 15:19:45 +0100

    AI Overhaul Navmesh

---
+++ Project-Time-Sweeper/Assets/Scripts/Guard.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    Guard curTarget;
+    Team myTeam;
+    Vitals myVitals;
+
+    Transform myTransform;
+
+    Animator anim;
+    public enum ai_states
+    {
+        idle,
+        move,
+        combat,
+    }
+
+    public ai_states states = ai_states.idle;
+
+    void Start()
+    {
+        myTransform = transform;
+        myTeam = GetComponent<Team>();
+        myVitals = GetComponent<Vitals>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(myVitals.GetCurrentHealth() > 0)
+        {
+            switch(states)
+            {
+                case ai_states.idle:
+                    stateIdle();
+                    break;
+                case ai_states.move:
+                    stateMove();
+                    break;
+                case ai_states.combat:
+                    stateCombat();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void stateIdle()
+    {
+
+    }
+
+    void stateMove()
+    {
+
+    }
+
+    void stateCombat()
+    {
+
+    }
+}
---
+++ Project-Time-Sweeper/Assets/Scripts/Vitals.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vitals : MonoBehaviour
+{
+    [SerializeField] float health;
+
+    float curHealth;
+    void Start()
+    {
+        curHealth = health;
+    }
+
+    public float GetCurrentHealth()
+    {
+        return curHealth;
+    }
+
+    public void getHit(float damage)
+    {
+        curHealth -= damage;
+    }
+}
